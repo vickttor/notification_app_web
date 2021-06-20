3f3c6de6d4b1d32cf1ba0cc6ef9b5d58416abea8
VictorSilva15 <victor.silvaPC@hotmail.com>
2021-06-20 20:51:45 -0300

    App updated

--- src/App.js
+++ src/App.js
@@ -2,25 +2,13 @@ import { Notifyer } from "./Notifyer.js";
 import { Timer } from "./Timer.js";
 import { Emitter } from "./Emitter.js";
 
-let mess = [
-  {
-    title: "Estudar JavaScript",
-    body: "Entre no canal do Myke e aprenda a Programar já!",
-  },
-  {
-    title: "Estudar PHP",
-    body: "Leia o livro de PHP e MySQL e faça os desafios que tu tem para fazer!",
-  },
-  {
-    title: "Estudar CSS",
-    body: "Entre no Youtube e produza páginas web responsivas e bonitas!",
-  },
-];
-
-const notify = Notifyer.notify(mess[Math.floor(Math.random() * mess.length)]);
+const notify = Notifyer.notify({
+  title: "Insta Post",
+  body: "Crie um novo conteúdo para ajudar a comunidade",
+});
 
 const App = {
-  async start() {
+  async start(time) {
     try {
       await Notifyer.init();
 
@@ -28,7 +16,7 @@ const App = {
 
       Emitter.on("countdown-end", Timer.init);
 
-      Timer.init(0.5 * 60);
+      Timer.init(time);
     } catch (err) {
       console.log(err.message);
     }
